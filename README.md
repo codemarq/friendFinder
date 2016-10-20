2b652834d297c3d354ced877a9e1705d25160ee3
John-Mike Marquardt <jmmarquardt@gmail.com>
2016-10-20 00:00:15 -0400

    survey questions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # friendFinder
 FullStack "Friend Finder" app using express, node.js, HTML, CSS
+
+## Screenshot
+
+Home Page
+
+![ScreenShot]('screenshot-localhost 3010-2016-10-19-21-30-00.png')
+
+
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,6 +1,18 @@
 module.exports = function (app) {
 	// dependencies
 	var path = require('path');
+	var friends = require('../data/friends.js');
 
+	// ================================================================
 	// api-routes
+	// ================================================================
+
+	// 
+	app.get('/api/friends', function (request, response) {
+		response.status(200).json({message: 'Successfully connected!'});
+		response.json(friends);
+	});
+
+
+
 };
